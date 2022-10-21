6f2ba13a5c14965a4cc497060ddf554f3e88f33c
Jose Ascurra <joseascurra123@gmail.com>
2022-10-21 00:33:02 -0300

    Agregado del numero de telefono en el recurso del cliente

--- app/Http/Resources/ClientResource.php
+++ app/Http/Resources/ClientResource.php
@@ -18,6 +18,7 @@ class ClientResource extends JsonResource
             'id' => $this->resource->id,
             'name' => $this->resource->name,
             'email' => $this->resource->email,
+            'phone_number' => $this->resource->phone_number,
             'created_at' => $this->resource->created_at->format('Y-m-d H:i:s'),
         ];
     }
